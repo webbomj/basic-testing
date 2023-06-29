35caab4e4021366e1396624033115180cfd568a2
webbomj <nethack@mail.ru>
2023-06-30 01:45:52 +0300

    feat: add 08 task

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,14 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const result = { value: 1, next: { value: null, next: null } };
+    expect(generateLinkedList([1])).toStrictEqual(result);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([2])).toMatchSnapshot();
   });
 });
